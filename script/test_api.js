cdb0390524e894292a45ff026c5a3bd177d1d5b1
Yinlianlei <1114561520@qq.com>
2021-10-11 21:56:40 +0800

    Site updated: 2021-10-11 21:56:40

--- script/test_api.js
+++ script/test_api.js
@@ -1,9 +1,5 @@
  function sendmsg(){
     console.log("????????");
 
-    var Hexo = require('hexo');
-
-    var hexo = new Hexo(process.cwd(), {});
-
-    console.log(hexo.route.list());
+    console.log("?>?>?");
 }
